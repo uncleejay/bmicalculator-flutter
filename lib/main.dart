3a1ce62597aa6f359d4d737bee196933f54a1a70
uncleejay <joseph.c.o216@gmail.com>
2021-09-07 00:28:55 +0100

    added shots

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class BMICalculator extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
             primaryColor: Color(0xFF0A0E21),
             scaffoldBackgroundColor: Color(0xFF0A0E21),
-            
+
       ),
       home: InputPage(),
     );
